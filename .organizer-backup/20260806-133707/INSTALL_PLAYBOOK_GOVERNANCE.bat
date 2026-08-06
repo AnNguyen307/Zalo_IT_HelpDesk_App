557410049f5077e0259a682b581f06ae0616216c
Anlozer123 <nguyenphantruongan123@gmail.com>
2026-08-06 14:07:57 +0700

    Remove local backup folders from repository

--- .organizer-backup/20260806-133707/INSTALL_PLAYBOOK_GOVERNANCE.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File ".\scripts\windows\install-playbook-governance.ps1" %*
-set EXIT_CODE=%ERRORLEVEL%
-if not "%EXIT_CODE%"=="0" pause
-exit /b %EXIT_CODE%
